c7ac63d3bb547ad95f3a40c36b38c698b23aaf92
류상욱 <ryuinkyoto@gmail.com>
2020-08-19 20:27:53 +0900

    Add: Nav바 레이아웃 완료

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,6 +3,54 @@ import "./Nav.scss";
 
 export default class Nav extends Component {
   render() {
-    return <div className="Nav"></div>;
+    return (
+      <nav className="Nav">
+        <div className="navContainer">
+          <ul className="navLeft">
+            <li className="logo">
+              <a href="/">
+                <img
+                  class="logo"
+                  alt="logo"
+                  src="/images/nav/watcha_logo.png"
+                />
+              </a>
+            </li>
+            <li className="movie">
+              <button className="movieBtn" name="movieBtn">
+                영화
+              </button>
+            </li>
+            <li className="tv">
+              <button className="tvBtn">TV 프로그램</button>
+            </li>
+            <li className="book">
+              <button className="bookBtn">책</button>
+            </li>
+          </ul>
+          <ul className="navRight">
+            <li className="search">
+              <div className="searchContainer">
+                <div className="label">
+                  <input
+                    id="searchInput"
+                    name="search"
+                    type="search"
+                    placeholder="작품 제목, 배우, 감독을 검색해보세요."
+                    autoComplete="off"
+                  ></input>
+                </div>
+              </div>
+            </li>
+            <li className="login">
+              <button className="loginBtn">로그인</button>
+            </li>
+            <li className="signUp">
+              <button className="signUpBtn">회원가입</button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
