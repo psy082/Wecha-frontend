45db63d73443da79403fd7947ef7863daa956de6
류상욱 <ryuinkyoto@gmail.com>
2020-08-20 15:27:36 +0900

    Modfiy: main.js에 Nav component 추가

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Nav from "../../Components/Nav/Nav";
-import Footer from "../../Components/Footer/Footer";
+// import Footer from "../../Components/Footer/Footer";
 import "./Main.scss";
 
 export default class Main extends Component {
   render() {
     return (
       <div className="Main">
-        <div className="contents">contents</div>
+        <Nav />
+        <div className="contents"></div>
       </div>
     );
   }
