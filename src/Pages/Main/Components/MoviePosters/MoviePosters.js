eeb26185a7858028f3af22883952853d4c0a0882
박주엽 <wnduq8@gmail.com>
2020-08-21 16:05:05 +0900

    Add: React Slick 적용/Custom Button 적용 - MainPage "왓챠 영화 순위" 슬라이더 적용 - Custom Button 적용 및 레이아웃 작성

--- src/Pages/Main/Components/MoviePosters/MoviePosters.js
+++ src/Pages/Main/Components/MoviePosters/MoviePosters.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import MoviePosterItem from "../MoviePosterItem/MoviePosterItem";
+import Slider from "react-slick";
+import SliderBtn from "../SliderBtn/SliderBtn";
 import "./MoviePosters.scss";
 
 export default class MoviePosters extends Component {
@@ -21,22 +23,35 @@ export default class MoviePosters extends Component {
   };
 
   render() {
+    const settings = {
+      dots: false,
+      infinite: false,
+      prevArrow: <SliderBtn to="prevArrow" />,
+      nextArrow: <SliderBtn to="nextArrow" />,
+      speed: 800,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      initialSlide: 0,
+    };
+
     return (
       <div className="MoviePosters">
         <ul className="moviePoster">
-          {this.state.data.map((el) => {
-            return (
-              <MoviePosterItem
-                key={el.id}
-                korean_title={el.korean_title}
-                poster_url={el.poster_url}
-                year={el.year}
-                country={el.country[0]}
-                avg_score={el.avg_score}
-                service_provider={el.service_provider}
-              />
-            );
-          })}
+          <Slider {...settings}>
+            {this.state.data.map((el) => {
+              return (
+                <MoviePosterItem
+                  key={el.id}
+                  korean_title={el.korean_title}
+                  poster_url={el.poster_url}
+                  year={el.year}
+                  country={el.country[0]}
+                  avg_score={el.avg_score}
+                  service_provider={el.service_provider}
+                />
+              );
+            })}
+          </Slider>
         </ul>
       </div>
     );
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -3,6 +3,8 @@ import Nav from "../../Components/Nav/Nav";
 import MainContainer from "./Components/MainContainer/MainContainer";
 import Footer from "../../Components/Footer/Footer";
 import "./Main.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default class Main extends Component {
   render() {
