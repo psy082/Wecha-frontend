6b1deaad2cdc86ac5be17e6a8b329fc186a48efd
류상욱 <ryuinkyoto@gmail.com>
2020-08-26 20:06:41 +0900

    Fix: 컨플릭트 수정

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import Nav from "../../Components/Nav/Nav";
 import "./Main.scss";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 export default class Main extends Component {
   render() {
